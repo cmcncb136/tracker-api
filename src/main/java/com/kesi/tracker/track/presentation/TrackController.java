ffa8654027f4fa37d5d5bf3654c3768b6b4aae8a
KJW <cmcncb13624@gmail.com>
2026-01-29 21:32:33 +0900

    feat. TrackController 조회 부분 구현

--- src/main/java/com/kesi/tracker/track/presentation/TrackController.java
+++ src/main/java/com/kesi/tracker/track/presentation/TrackController.java
@@ -1,15 +1,49 @@
 package com.kesi.tracker.track.presentation;
 
 
+import com.kesi.tracker.core.security.annotation.UserId;
 import com.kesi.tracker.track.application.TrackService;
+import com.kesi.tracker.track.application.dto.TrackResponse;
+import com.kesi.tracker.track.application.dto.TrackSearchRequest;
+import com.kesi.tracker.track.application.dto.TrackWithGroupResponse;
+import com.kesi.tracker.track.application.dto.TrackWithGroupSearchRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("track")
 public class TrackController {
     private final TrackService trackService;
 
+    @GetMapping("/groups/tracks/{trackId}")
+    public TrackResponse getTrack(
+            @PathVariable("trackId") Long trackId,
+            @UserId Long userId
+            ) {
+
+        return trackService.getTrackResponseById(trackId, userId);
+    }
+
+    @GetMapping("/groups/tracks")
+    public Page<TrackResponse> searchTrackInGroup(
+            @UserId Long userId,
+            @RequestParam Long gid,
+            @ModelAttribute TrackSearchRequest searchRequest,
+            @PageableDefault Pageable pageable
+            ) {
+        return trackService.searchTrackInGroup(gid, userId, searchRequest, pageable);
+    }
+
+    @GetMapping("/users/tracks")
+    public Page<TrackWithGroupResponse> searchTrackInUser(
+            @UserId Long userId,
+            @ModelAttribute TrackWithGroupSearchRequest searchRequest,
+            @PageableDefault Pageable pageable
+    ) {
+        return trackService.searchTrackInGroupInUser(userId, searchRequest, pageable);
+    }
 }
